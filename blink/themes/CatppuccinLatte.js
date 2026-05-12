fdadd44eb2d38313b1caf53546201bec15c8da6c
Suresh Raja <suresh@macpro.lan>
2026-05-11 21:08:09 -0500

    Add Catppuccin Latte theme for Blink Shell

---
+++ blink/themes/CatppuccinLatte.js
@@ -0,0 +1,22 @@
+t.prefs_.set('color-palette-overrides', [
+  '#5c5f77', // black
+  '#d20f39', // red
+  '#40a02b', // green
+  '#df8e1d', // yellow
+  '#1e66f5', // blue
+  '#ea76cb', // magenta
+  '#179299', // cyan
+  '#acb0be', // white
+  '#6c6f85', // bright black
+  '#de293e', // bright red
+  '#49af3d', // bright green
+  '#eea02d', // bright yellow
+  '#456eff', // bright blue
+  '#fe85d8', // bright magenta
+  '#2d9fa8', // bright cyan
+  '#bcc0cc', // bright white
+]);
+t.prefs_.set('background-color', '#eff1f5');
+t.prefs_.set('foreground-color', '#4c4f69');
+t.prefs_.set('cursor-color', '#dc8a78');
+t.prefs_.set('cursor-text-color', '#eff1f5');
